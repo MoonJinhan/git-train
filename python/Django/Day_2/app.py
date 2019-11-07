a5525b1da656e19724f821592140b2d83c6f9bc0
MoonJinhan <50944888+MoonJinhan@users.noreply.github.com>
2019-11-07 16:25:36 +0900

    update

--- python/Django/Day_2/app.py
+++ python/Django/Day_2/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import random
+ 
 
 app = Flask(__name__)
 
@@ -20,7 +21,7 @@ def greeting(name):
 def lunch(num):
     menu=["짜장면","짜마뽕","공기밥","짜장면곱배기","진한국밥"]
     order = random.sample(menu,num)
-    return str(order)
+    return render_template('menu.html', menu=order)
 
 @app.route('/html')
 def html():
@@ -34,5 +35,47 @@ def html():
 def hi(name):
     return render_template('hi.html',name=name)
 
+@app.route('/fake_naver')
+def fake_naver():
+    return render_template('fake_naver.html')
+
+@app.route('/fake_google')
+def fake_google():
+    return render_template('fake_google.html')
+
+@app.route('/send')
+def send():
+    return render_template('send.html')
+
+@app.route('/receive')
+def receive():
+    name=request.args.get('name')
+    message = request.args.get('message')
+    return render_template('receive.html',name=name,msg=message)
+
+@app.route('/birth')
+def birth():
+    return render_template('birth.html')
+
+@app.route('/indian1')
+def indian():
+    year = request.args.get('year')
+    month = request.args.get('month')
+    date =request.args.get('date')
+    return render_template('indian.html', year=year,month=month,date=date)
+
+@app.route('/indian')
+def indian():
+    return render_template(indian1.html):
+
+@app.route('/indian_result')
+def result():
+    list_1=['말 많은'],
+    list_2=['늑대'],
+    list_3=['~와 함께 춤을']
+
+    l1 =random.choice(list_1)
+    l2 = random.choice(list_2)
+    l3 = random.choice(list_3)
 if __name__=="__main__":
     app.run(debug=True, port=8000)
