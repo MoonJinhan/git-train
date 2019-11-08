54196db1d4eafe4aa630db5f1758e53b073bce89
MoonJinhan <50944888+MoonJinhan@users.noreply.github.com>
2019-11-08 17:31:19 +0900

    Update set_webhook.py

--- python/Django/Day_3/set_webhook.py
+++ python/Django/Day_3/set_webhook.py
@@ -5,8 +5,8 @@ from pprint import pprint
 token=config('TOKEN')
 base_url=f"https://api.telegram.org/bot{token}"
 
-
-url='f7393776.ngrok.io'
+url="answls7337.pythonanywhere.com/"
+# url='f7393776.ngrok.io'
 setweb_url = f'/setWebhook?url={url}'
 
 req =requests.get(base_url+setweb_url).json()
