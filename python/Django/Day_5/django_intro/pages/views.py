bda47e39319809e3c6ceb084e7bba5687711379c
MoonJinhan <50944888+MoonJinhan@users.noreply.github.com>
2019-11-13 19:53:28 +0900

    up

---
+++ python/Django/Day_5/django_intro/pages/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from pprint import pprint
+import random
+
+def throw(request):
+    return render(request,'pages/throw.html')
+
+def catch(request):
+    # pprint(request)
+    # pprint(request.path)
+    # pprint(request.method)
+    # pprint(request.META)
+    message = request.GET.get('message')
+    message2 = request.GET.get('message')
+    context={
+        'msg':message,
+        'msg2':message2
+    }
+    return render(request, 'pages/catch.html', context)
+
+def lotto(request):
+    return render(request,'pages/lotto.html')
+
+def lottoresult(request):
+    co = request.GET.get('count')
+    # request로 넘어오는 값 중 GET방식의 count 를 얻을 것이다.
+    print(type(co))
+
+    count=int(request.GET.get('count')) #형병환 해주는 코드
+    lotto_num=[]
+    for i in range(count):
+        lotto_num.append(random.sample)
+
