928ea5e345cfcf5979170f9761f9b6e7343b9d55
MoonJinhan <50944888+MoonJinhan@users.noreply.github.com>
2019-11-14 13:09:50 +0900

    up

--- python/Django/Day_7/django_orm/boards/models.py
+++ python/Django/Day_7/django_orm/boards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Boards(models.Model):
+    title=models.CharField(max_length=10)
+    content = models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    #auto_now_add 해당 데이터가 생성을 할 때 자동으로 시간을 만들어줌.
+    updated_at = models.DateTimeField(auto_now=True)
+    #auto_now 해당 데이터가 수정을 할 때 자동으로 시간을 만들어줌.
+
+    def __str__(self):
+        return f'{self.id} : {self.title}'
+    #make 마이크래이션을 해줄 필요가 없다. 왜냐하면 ㅅ
+
+
