99c2b083222743bfecf4aea38c3acb7cc00b2625
MoonJinhan <50944888+MoonJinhan@users.noreply.github.com>
2019-11-14 10:13:01 +0900

    fix

--- python/Django/Day_7/django_orm/config/urls.py
+++ python/Django/Day_7/django_orm/config/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('borads/',include('boards.url')),
+    path('borads/',include('boards.urls')),
     path('admin/', admin.site.urls),
 ]
