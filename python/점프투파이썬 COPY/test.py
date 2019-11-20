77a5124939b21a89e6f66d0011b859ccac420db8
Moonjinhan <answls7337@naver.com>
2019-11-20 23:45:26 +0900

    20191120 self_study

--- python/점프투파이썬 COPY/test.py
+++ python/점프투파이썬 COPY/test.py
@@ -1,9 +1,19 @@
-grade ={'pey':10,'julliet':99}
-print(grade.keys())
-print(list((grade.keys())))
+# grade ={'pey':10,'julliet':99}
+# print(grade.keys())
+# print(list((grade.keys())))
 
-print(grade['pey'])
+# print(grade['pey'])
 
-a={1:'a',2:'b'}
-print(a[1])
+# a={1:'a',2:'b'}
+# print(a[1])
 
+# s1=set([1,2,3,4])
+# l1 =list(s1)
+# print(l1)
+# print(l1[0])
+
+s2 = set("moonjinhan")
+l2 = list(s2)
+print(l2)
+
+print(l2[0])
