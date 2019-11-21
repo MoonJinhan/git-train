2fe4bf67a49f3f8e1c622b2a5d3277e015d7dfee
Moonjinhan <answls7337@naver.com>
2019-11-21 23:51:45 +0900

    20191121

--- python/점프투파이썬 COPY/test.py
+++ python/점프투파이썬 COPY/test.py
@@ -1,3 +1,32 @@
+
+#<<<<변수>>>>
+#리스트를 복사하는 방법
+# a=[1,2,3]
+# b=a[:]
+# a[1]=4
+# print(a)
+# print(b)
+
+from copy import copy
+
+a=[1,2,3]
+b=[1,2,3]
+# b=copy(a)
+# print(b)
+print(b is a)
+#<<<<bool>>>>
+# if [1]:
+#     print("참")
+# else:
+#     print("거짓")
+# # a=True
+# b=False
+
+# print(type(a))
+# print(1==1)
+
+#<<<<dic>>>>
+# print(())
 # grade ={'pey':10,'julliet':99}
 # print(grade.keys())
 # print(list((grade.keys())))
@@ -7,13 +36,15 @@
 # a={1:'a',2:'b'}
 # print(a[1])
 
+#<<<<list>>>>
 # s1=set([1,2,3,4])
 # l1 =list(s1)
 # print(l1)
 # print(l1[0])
 
-s2 = set("moonjinhan")
-l2 = list(s2)
-print(l2)
+# s2 = set("moonjinhan")
+# l2 = list(s2)
+# print(l2)
 
-print(l2[0])
+# print(l2[0])
+
