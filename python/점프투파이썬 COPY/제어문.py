edd632e198350ac0647bcb9f8dd33003fc675720
Moonjinhan <answls7337@naver.com>
2019-11-24 01:28:06 +0900

    20191123

---
+++ python/점프투파이썬 COPY/제어문.py
@@ -0,0 +1,39 @@
+#<<<<if>>>>
+# m =True
+
+# if m:
+#     print('택스를 타자')
+# else:
+#     print('돈이 없다.')
+
+#<<<<and, or, not>>>>
+
+# m=2000
+# card =True
+
+# if m >= 5000 or card:
+#     print(1)
+# else:
+#     print(2)
+
+#<<<<in>>>>
+
+# print('j' not in 'python')
+
+# p = ['A','B','C']
+# if 'A' in p:
+#     print('있다.')
+# else:
+#     print('없다')
+
+#<<<elif>>>
+
+p = ['a','b']
+card = True
+
+if 'm' in p:
+    print("택시를 타고가라")
+elif card:
+    print("역시 택시를 타고 가라")
+else:
+    print("기어가라 영주")
