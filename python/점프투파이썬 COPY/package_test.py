bdf3f832c66ea846313b925b0d6bada5689b8cfb
Moonjinhan <answls7337@naver.com>
2019-12-30 00:09:38 +0900

    Create package_test.py

---
+++ python/점프투파이썬 COPY/package_test.py
@@ -0,0 +1,17 @@
+game/
+
+    __init__.py
+    sound/
+        __init__.py
+        echo.py
+        wav.py
+
+    graphic/
+        __init__.py
+        screen.py
+        render.py
+        
+    play/
+        __init__.py
+        run.py
+        test.py
