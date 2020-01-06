392c27e34db7e2535e2345352f7327ce475efcfa
Moonjinhan <answls7337@naver.com>
2020-01-06 22:41:28 +0900

    20200106

---
+++ python/점프투파이썬 COPY/내장함쑤.py
@@ -0,0 +1,21 @@
+# print(abs(-3.1))
+# abc 절댓값을 돌려주는 함수
+
+# print(all(1))
+# TypeError: 'int' object is not iterable
+#궁금해서 리스트형을 지우고 int 형 1 하나만 넣어줬다. 그랬더니 에러 ..
+
+# print(all([1,2,3,4]))
+# True
+
+# all(x)는 반복 가능한(iterable) 자료형 x를 입력 인수로 받으며 이 x가 모두 참이면 True, 거짓이 하나라도 있으면 False를 돌려준다.
+# ※ 반복 가능한 자료형이란 for문으로 그 값을 출력할 수 있는 것을 의미한다. 리스트, 튜플, 문자열, 딕셔너리, 집합 등이 있다
+
+#any(x)는 x 중 하나라도 참이 있으면 True를 돌려주고, x가 모두 거짓일 때에만 False를 돌려준다. all(x)의 반대이다.
+# print(any([1,2,3,0]))
+
+# print(chr(97))
+# a
+
+# print(dir([1,2,3]))
+
